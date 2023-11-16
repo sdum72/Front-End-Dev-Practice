0ebff31fb2c46fa5c22e768b9f8b20110221c7a2
sdum72 <sdum@hotmail.com>
2023-11-16 10:18:38 +0000

    added functions code to script.js file in 03-Stu-Function/starter

--- UK-VIRT-FE-PT-10-2023-U-LOLC-main/05-advanced-javascript-module/02-functions-lesson/03-Stu-Functions/starter/script.js
+++ UK-VIRT-FE-PT-10-2023-U-LOLC-main/05-advanced-javascript-module/02-functions-lesson/03-Stu-Functions/starter/script.js
@@ -1,1 +1,20 @@
 // Write Your Code Below
+
+function addNumbers() {
+    return 1 + 2;
+}
+
+console.log(addNumbers())
+
+function subtractNumbers() {
+    return 20 - 10;
+}
+
+console.log(subtractNumbers())
+
+var side = 5;
+function findArea() {
+    return side * side;
+}
+
+console.log(findArea(side))
